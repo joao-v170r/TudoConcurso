3f846ed48f44df722c5fd0796a6ee801845e9f8b
JoaoVitorAguiar <joaovitoraguiarsillva@gmail.com>
2022-05-05 09:48:05 -0300

    Adicionando Classes é nova estrutura de arquivos

---
+++ src/_Class/Usuario.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Usuario {
+    public readonly string $dtNascimento;
+    public readonly string $cpf;
+    public readonly int $idUser;
+    private string $nome;
+    private string $cep;
+    private string $email;
+    private array $perfilConcurso;
+    private array $bancaCharge;
+
+    public function __construct(string $dtNascimento, string $cpf, int $idUser, string $nome, string $cep, string $email){
+        $this->$dtNascimento = $dtNascimento;
+        $this->$cpf = $cpf;
+        $this->$idUser = $idUser;
+        $this->$nome = $nome;
+        $this->$cep = $cep;
+        $this->$email = $email;
+    }
+
+}
