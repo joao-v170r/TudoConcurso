2512847d07d900c5f7207bf4ded3dbe73a4937b4
victornaves1810 <104800774+victornaves1810@users.noreply.github.com>
2022-05-30 18:22:10 -0300

    Atualização do header
    Atualização do header para incluir stylesheet do formulário

--- src/header.php
+++ src/header.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="../assets/css/menu.css">
+<link rel="stylesheet" href="../assets/css/form.css">
 <title>Tudo Concurso</title>
 </head>
 <body>
