6460e99c646676a814f8324a5d5e8938d31902f3
victornaves1810 <victor.naves1810@gmail.com>
2022-06-06 20:28:59 -0300

    separando questões por nome

--- src/form.php
+++ src/form.php
@@ -10,91 +10,91 @@
     <h1 class="pageTitle">Questionário</h1>
     <p class="question">Para qual dessas áreas você prefere prestar concurso público?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Judiciária</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Segurança</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Magistério</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Administrativa</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Fiscal</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao1" /> Judiciária</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao1" /> Segurança</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao1" /> Magistério</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao1" /> Administrativa</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao1" /> Fiscal</label>
     </div>
     <p class="question">Para qual dessas áreas você prefere prestar concurso público?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Militar</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Controle</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Legislativa</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Bancária</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Alta Gestão</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao2" /> Militar</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao2" /> Controle</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao2" /> Legislativa</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao2" /> Bancária</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao2" /> Alta Gestão</label>
     </div>
     <p class="question">Em qual região você mora?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Norte</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Nordeste</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Sul</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Sudeste</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Centro-oeste</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao3" /> Norte</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao3" /> Nordeste</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao3" /> Sul</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao3" /> Sudeste</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao3" /> Centro-oeste</label>
     </div>
     <p class="question">Qual é o seu nível de escolaridade?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Ensino médio completo</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Ensino superior incompleto</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Ensino superior completo</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Pós graduação em andamento</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Pós graduado</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao4" /> Ensino médio completo</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao4" /> Ensino superior incompleto</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao4" /> Ensino superior completo</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao4" /> Pós graduação em andamento</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao4" /> Pós graduado</label>
     </div>
     <p class="question">Por qual área do conhecimento você se interessa mais?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Ciências Exatas</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Ciências Humanas</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Direito</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Ciências Biológicas</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Engenharia</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao5" /> Ciências Exatas</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao5" /> Ciências Humanas</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao5" /> Direito</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao5" /> Ciências Biológicas</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao5" /> Engenharia</label>
     </div>
     <p class="question">Você trabalharia fora da região em que você mora atualmente?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Sim</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Provavelmente sim</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Não sei</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Provavelmente não</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Não</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao6" /> Sim</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao6" /> Provavelmente sim</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao6" /> Não sei</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao6" /> Provavelmente não</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao6" /> Não</label>
     </div>
     <p class="question">Qual é a sua pretensão salarial?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> R$1.000 - R$2.000</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> R$2.000 - R$5.000</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> R$5.000 - R$8.000</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> R$8.000 - R$11.000</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Mais que R$ 11.000</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao7" /> R$1.000 - R$2.000</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao7" /> R$2.000 - R$5.000</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao7" /> R$5.000 - R$8.000</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao7" /> R$8.000 - R$11.000</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao7" /> Mais que R$ 11.000</label>
     </div>
     <p class="question">Com qual tipo de material de estudo você tem mais afinidade?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Apostilas</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Vídeo-aulas</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Cursos presenciais</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Cursos à distância</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Provas antigas</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao8" /> Apostilas</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao8" /> Vídeo-aulas</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao8" /> Cursos presenciais</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao8" /> Cursos à distância</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao8" /> Provas antigas</label>
     </div>
     <p class="question">Sobre qual tipo de concurso você deseja se informar?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Concurso público municipal</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Concurso público estadual</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Concurso público federal</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Concurso público distrital</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Todos</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao9" /> Concurso público municipal</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao9" /> Concurso público estadual</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao9" /> Concurso público federal</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao9" /> Concurso público distrital</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao9" /> Todos</label>
     </div>
     <p class="question">Qual carga horária de trabalho seeria ideal para você?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> 20h por semana</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> 30h por semana</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> 40h por semana</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Mais de 40h por semana</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Disponibilidade para plantões</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao10" /> 20h por semana</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao10" /> 30h por semana</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao10" /> 40h por semana</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao10" /> Mais de 40h por semana</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao10" /> Disponibilidade para plantões</label>
     </div>
     <p class="question">Você tem disponibilidade para realizar provas em outras cidades?</p>
     <div class="question-answer">
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Sim</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Provavelmente sim</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Não sei</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Provavelmente não</label>
-      <label><input class="pageInput" type="radio" value="none" name="visited" /> Não</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao11" /> Sim</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao11" /> Provavelmente sim</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao11" /> Não sei</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao11" /> Provavelmente não</label>
+      <label><input class="pageInput" type="radio" value="none" name="questao11" /> Não</label>
     </div>
   </form>
 </div>
