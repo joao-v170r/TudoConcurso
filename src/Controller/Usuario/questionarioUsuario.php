784b735d523039762b27d9994d6986e399c8b52f
victornaves1810 <victor.naves1810@gmail.com>
2022-06-13 20:17:59 -0300

    alterações no css e controlller do questionário

--- src/Controller/Usuario/questionarioUsuario.php
+++ src/Controller/Usuario/questionarioUsuario.php
@@ -1,22 +1,17 @@
 <?php
-namespace TudoConcurso\Controller\Usuario;
+namespace TudoConcurso\Controller\Questionario;
 
-$questaoA = isset($_POST['questao1']) ? $_POST['questao1'] : '';
-$questaoB = isset($_POST['questao2']) ? $_POST['questao2'] : '';
-$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
-$questaoC = isset($_POST['questao4']) ? $_POST['questao3'] : '';
-$questaoC = isset($_POST['questao5']) ? $_POST['questao3'] : '';
-$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
-$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
-$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
+    $questaoA = isset($_POST['questao1']) ? $_POST['questao1'] : '';
+    $questaoB = isset($_POST['questao2']) ? $_POST['questao2'] : '';
+    $questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
+    $questaoD = isset($_POST['questao4']) ? $_POST['questao4'] : '';
+    $questaoE = isset($_POST['questao5']) ? $_POST['questao5'] : '';
+    $questaoF = isset($_POST['questao6']) ? $_POST['questao6'] : '';
+    $questaoG = isset($_POST['questao7']) ? $_POST['questao7'] : '';
+    $questaoH = isset($_POST['questao8']) ? $_POST['questao8'] : '';
+    $questaoI = isset($_POST['questao9']) ? $_POST['questao9'] : '';
+    $questaoJ = isset($_POST['questao10']) ? $_POST['questao10'] : '';
+    $questaoK = isset($_POST['questao11']) ? $_POST['questao11'] : '';
 
- $objUser->nome = $nome;
- $objUser->cep = $cep;
- $objUser->email = $email;
- $objUser->dtNascimento = $dtNascimento;
- $objUser->senha = $senha;                 
- 
- $objUserDao->salvaUsuario($objUser);
- 
 
 ?>
