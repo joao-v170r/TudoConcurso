766a9338ffa71c2f0826662b816db395a98fec42
victornaves1810 <victor.naves1810@gmail.com>
2022-06-13 19:51:53 -0300

    consertei css do header

--- src/Controller/Usuario/questionarioUsuario.php
+++ src/Controller/Usuario/questionarioUsuario.php
@@ -4,6 +4,11 @@ namespace TudoConcurso\Controller\Usuario;
 $questaoA = isset($_POST['questao1']) ? $_POST['questao1'] : '';
 $questaoB = isset($_POST['questao2']) ? $_POST['questao2'] : '';
 $questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
+$questaoC = isset($_POST['questao4']) ? $_POST['questao3'] : '';
+$questaoC = isset($_POST['questao5']) ? $_POST['questao3'] : '';
+$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
+$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
+$questaoC = isset($_POST['questao3']) ? $_POST['questao3'] : '';
 
  $objUser->nome = $nome;
  $objUser->cep = $cep;
