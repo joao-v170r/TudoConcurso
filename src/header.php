5f2b66ef90e01cf0180a1b9a9d5535bad80b84af
Joao-Vitor-Aguiar <joaovitoraguiarsilva@gmail.com>
2022-06-20 21:24:45 -0300

    Adicionando novo css e divda navBar

--- src/header.php
+++ src/header.php
@@ -13,7 +13,6 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="../assets/css/menu.css">
-<link rel="stylesheet" href="../assets/css/questionario.css">
 <link rel="stylesheet" href="../assets/css/style.css">
 <title>Tudo Concurso</title>
 </head>
@@ -49,3 +48,4 @@
         </div>
     </div>
 </nav>
+<div class="display-block-navBar"></div>
